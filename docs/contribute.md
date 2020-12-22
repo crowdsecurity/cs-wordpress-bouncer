45074c7e1e8ed7f6910187cb8ea4adb321b949d1
Lucas Cherifi <lucas.cherifi@gmail.com>
2020-12-22 16:11:57 +0100

    fix release process

--- docs/contribute.md
+++ docs/contribute.md
@@ -96,12 +96,12 @@ git push origin :<old-name> && git push origin <new-name>
 gh pr create --fill
 ```
 
-After the merge, don't forget to delete to branch.
+> Note: after the merge, don't forget to delete to branch.
 
 #### New release
 
 ```bash
-git checkout main && git pull
+git checkout main && git pull && git co -
 git describe --tags `git rev-list --tags --max-count=1` # to verify what is the current tag
 export NEW_GIT_VERSION_WITHOUT_V_PREFIX= #...X.X.X
 ./scripts/publish-release.sh
--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -18,22 +18,22 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
 fi
 git_base_dir=`git rev-parse --show-toplevel`
 
-# Update version everywhere (add and commit changes)
+# Update version everywhere (add and commit changes), tag and release
+git checkout main
 if [[ $platform == 'linux' ]]; then
-   sed -i -E "s/Version [0-9]+\.[0-9]+\.[0-9]/Version $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" `git rev-parse --show-toplevel`/crowdsec.php
-   sed -i -E "s/Stable tag: [0-9]+\.[0-9]+\.[0-9]/Stable tag: $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" `git rev-parse --show-toplevel`/crowdsec.php
-   sed -i -E "s/v[0-9]+\.[0-9]+\.[0-9]/v$NEW_GIT_VERSION_WITHOUT_V_PREFIX/" `git rev-parse --show-toplevel`/inc/constants.php
+   sed -i -E "s/Version [0-9]+\.[0-9]+\.[0-9]/Version $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" $git_base_dir/crowdsec.php
+   sed -i -E "s/Stable tag: [0-9]+\.[0-9]+\.[0-9]/Stable tag: $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" $git_base_dir/crowdsec.php
+   sed -i -E "s/v[0-9]+\.[0-9]+\.[0-9]/v$NEW_GIT_VERSION_WITHOUT_V_PREFIX/" $git_base_dir/inc/constants.php
 else
-   sed -i "" -E "s/Version [0-9]+\.[0-9]+\.[0-9]/Version $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" `git rev-parse --show-toplevel`/crowdsec.php
-   sed -i "" -E "s/Stable tag: [0-9]+\.[0-9]+\.[0-9]/Stable tag: $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" `git rev-parse --show-toplevel`/crowdsec.php
-   sed -i "" -E "s/v[0-9]+\.[0-9]+\.[0-9]/v$NEW_GIT_VERSION_WITHOUT_V_PREFIX/" `git rev-parse --show-toplevel`/inc/constants.php
+   sed -i "" -E "s/Version [0-9]+\.[0-9]+\.[0-9]/Version $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" $git_base_dir/crowdsec.php
+   sed -i "" -E "s/Stable tag: [0-9]+\.[0-9]+\.[0-9]/Stable tag: $NEW_GIT_VERSION_WITHOUT_V_PREFIX/" $git_base_dir/crowdsec.php
+   sed -i "" -E "s/v[0-9]+\.[0-9]+\.[0-9]/v$NEW_GIT_VERSION_WITHOUT_V_PREFIX/" $git_base_dir/inc/constants.php
 fi
-git add `git rev-parse --show-toplevel`/inc/constants.php
-git add `git rev-parse --show-toplevel`/crowdsec.php
-git commit -m "bump version to v$NEW_GIT_VERSION_WITHOUT_V_PREFIX"
-echo "Note: new commit \"bump version to v$NEW_GIT_VERSION_WITHOUT_V_PREFIX\""
+git add $git_base_dir/inc/constants.php
+git add $git_base_dir/crowdsec.php
 
-# Tag and release
 git tag v$NEW_GIT_VERSION_WITHOUT_V_PREFIX
+git commit -m "bump version to v$NEW_GIT_VERSION_WITHOUT_V_PREFIX"
+git push
 git push origin v$NEW_GIT_VERSION_WITHOUT_V_PREFIX
 gh release create --draft v$NEW_GIT_VERSION_WITHOUT_V_PREFIX --title v$NEW_GIT_VERSION_WITHOUT_V_PREFIX
