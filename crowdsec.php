731492325e4aa6ac28f13561d444b65673fb44f0
Lucas Cherifi <lucas.cherifi@gmail.com>
2021-06-22 15:21:52 +0200

    fix bug when other plugins execute "session_start"

--- crowdsec.php
+++ crowdsec.php
@@ -17,7 +17,9 @@
  * Text Domain: crowdsec-wp
  * First release: 2021.
  */
-session_start();
+if (\PHP_SESSION_NONE === session_status()) {
+    session_start();
+}
 require_once __DIR__.'/vendor/autoload.php';
 
 define('CROWDSEC_PLUGIN_PATH', __DIR__);
