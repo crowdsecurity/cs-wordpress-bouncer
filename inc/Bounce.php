7c6eb9a7ab872563e4785dc758aa55791aa6a316
Lucas Cherifi <lucas.cherifi@gmail.com>
2021-06-23 23:23:53 +0200

    prevent proies from caching walls

--- inc/Bounce.php
+++ inc/Bounce.php
@@ -290,9 +290,15 @@ class Bounce extends AbstractBounce implements IBounce
                 break;
             case 401:
                 header('HTTP/1.0 401 Unauthorized');
+                header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+                header('Cache-Control: post-check=0, pre-check=0', false);
+                header('Pragma: no-cache');
                 break;
             case 403:
                 header('HTTP/1.0 403 Forbidden');
+                header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+                header('Cache-Control: post-check=0, pre-check=0', false);
+                header('Pragma: no-cache');
                 break;
             default:
                 throw new Exception("Unhandled code ${statusCode}");
