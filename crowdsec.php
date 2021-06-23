ccc7af9ed4cf379bf9c06dfff378d1a2464f95fe
Lucas Cherifi <lucas.cherifi@gmail.com>
2021-06-24 01:03:21 +0200

    indicate that wp5.7 is now tested

--- crowdsec.php
+++ crowdsec.php
@@ -12,7 +12,7 @@
  * License URI: https://opensource.org/licenses/MIT
  * Requires PHP: 7.2
  * Requires at least: 4.9
- * Tested up to: 5.6
+ * Tested up to: 5.7
  * Stable tag: 1.0.0
  * Text Domain: crowdsec-wp
  * First release: 2021.
