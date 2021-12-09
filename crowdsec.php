743f8e81d039c0fae450034e01e6cc9d7669a3a4
Julien Loizelet <julienloizelet@okaeli.com>
2021-12-09 10:53:22 +0900

    chore(*): bump version to v1.2.0

--- crowdsec.php
+++ crowdsec.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/crowdsecurity/cs-wordpress-bouncer
  * Description: Safer Together. Protect your WordPress application with CrowdSec.
  * Tags: crowdsec-bouncer, wordpress, security, firewall, captcha, ip-scanner, ip-blocker, ip-blocking, ip-address, ip-database, ip-range-check, crowdsec, ban-hosts, ban-management, anti-hacking, hacker-protection, captcha-image, captcha-generator, captcha-generation, captcha-service
- * Version: 1.1.2
+ * Version: 1.2.0
  * Author: CrowdSec
  * Author URI: https://www.crowdsec.net/
  * Github: https://github.com/crowdsecurity/cs-wordpress-blocker
@@ -13,7 +13,7 @@
  * Requires PHP: 7.2
  * Requires at least: 4.9
  * Tested up to: 5.7
- * Stable tag: 1.1.2
+ * Stable tag: 1.2.0
  * Text Domain: crowdsec-wp
  * First release: 2021.
  */
