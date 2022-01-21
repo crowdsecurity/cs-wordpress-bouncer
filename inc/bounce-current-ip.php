bd0b105aa2aee1a7d69583109cae5bfd1bb37dca
Julien Loizelet <julienloizelet@okaeli.com>
2022-01-21 17:39:36 +0100

    feat(config): Retrieve configurations from database if not standalone mode

--- inc/bounce-current-ip.php
+++ inc/bounce-current-ip.php
@@ -3,6 +3,19 @@
 use CrowdSecBouncer\Constants;
 
 require_once __DIR__ . '/Bounce.php';
+require_once __DIR__.'/options-config.php';
+
+function getConfigs()
+{
+	$crowdSecWpPluginOptions = getCrowdSecOptionsConfig();
+	$finalConfigs = [];
+	foreach ($crowdSecWpPluginOptions as $option) {
+		$finalConfigs[$option['name']] = get_option($option['name']);
+	}
+
+	return $finalConfigs;
+}
+
 
 function safelyBounceCurrentIp()
 {
@@ -13,14 +26,8 @@ function safelyBounceCurrentIp()
     if (\PHP_SESSION_NONE === session_status()) {
         session_start();
     }
-    if (!file_exists(__DIR__.'/standalone-settings.php')) {
-        return;
-    }
-    require_once __DIR__.'/standalone-settings.php';
-    $crowdSecConfig = json_decode($crowdSecJsonStandaloneConfig, true);
-    if (!count($crowdSecConfig)) {
-        return;
-    }
+
+	$crowdSecConfig = getConfigs();
 	// Retro compatibility with crowdsec php lib < 0.14.0
     if($crowdSecConfig['crowdsec_bouncing_level'] === 'normal_boucing'){
 		$crowdSecConfig['crowdsec_bouncing_level'] = Constants::BOUNCING_LEVEL_NORMAL;
