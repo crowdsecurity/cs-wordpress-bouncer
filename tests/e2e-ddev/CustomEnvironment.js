36c700b09f13f29de145cc0772757346f3d2f09b
Julien Loizelet <julienloizelet@okaeli.com>
2022-01-27 11:00:02 +0100

    ci(end to end): Remove some test due to php 7.4 incompatibility

--- tests/e2e-ddev/CustomEnvironment.js
+++ tests/e2e-ddev/CustomEnvironment.js
@@ -9,7 +9,11 @@ class CustomEnvironment extends PlaywrightEnvironment {
 				event.name === "test_fn_failure"
 			) {
 				this.failedTest = true;
-				const buffer = await this.global.page.screenshot();
+				const buffer = await this.global.page.screenshot({
+					path: "screenshot.jpg",
+					type: "jpeg",
+					quality: 20,
+				});
 				console.debug("Screenshot:", buffer.toString("base64"));
 			} else if (this.failedTest && event.name === "test_start") {
 				// eslint-disable-next-line no-param-reassign
